8985a476a6d586d2db4f9f673d80240bca69396c
Jorge <Jorge@DESKTOP-IVNUOSM.wifi.isec.pt>
2016-11-04 16:31:18 +0000

    Criei o cliente

---
+++ Cliente/src/cliente/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Tudo bem!");
+    }
+    
+}
