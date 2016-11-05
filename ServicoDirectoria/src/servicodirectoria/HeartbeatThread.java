ae0209a5dc7cb74e2f91f4eef53a0be7473b310a
Luis <Luis@DESKTOP-DQTU0QG.wifi.isec.pt>
2016-11-05 14:46:28 +0000

    Servidor criado

---
+++ ServicoDirectoria/src/servicodirectoria/HeartbeatThread.java
@@ -0,0 +1,19 @@
+package servicodirectoria;
+
+public class HeartbeatThread extends Thread {
+
+    public HeartbeatThread() {
+    }
+    
+    
+
+    @Override
+    public void run() {
+        super.run(); //To change body of generated methods, choose Tools | Templates.
+        
+        
+    }
+    
+    
+    
+}
