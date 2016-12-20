8772453e551270e3532f7a83689578fd96c53762
Jorge <Jorge@DESKTOP-IVNUOSM.wifi.isec.pt>
2016-12-20 01:17:14 +0000

    Servidor actualizado e comunicação cliente a arrancar

--- Server/HeartbeatThreadSend.java
+++ Server/HeartbeatThreadSend.java
@@ -1,8 +1,13 @@
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,30 +20,46 @@ public class HeartbeatThreadSend extends Thread{
     
     private DatagramSocket socketSend;
     private DatagramPacket packetSend;
+    InetAddress ip;
 
     public HeartbeatThreadSend(InetAddress ip) throws SocketException{
         socketSend = new DatagramSocket();
-        packetSend = new DatagramPacket(Constants.HEARTBEAT_SERVER.getBytes(),Constants.HEARTBEAT_SERVER.length()
-                                        ,ip, Constants.SENDING_PORT);
+        this.ip = ip;
+        //packetSend = new DatagramPacket(Constants.HEARTBEAT_SERVER.getBytes(),Constants.HEARTBEAT_SERVER.length(),ip, Constants.SENDING_PORT);
     }
     
     @Override
     public void run() {
         
-        if(socketSend == null){
-                return;
-        }
+         ByteArrayOutputStream byteStream = new ByteArrayOutputStream(Constants.MAX_SIZE);
         
-        while(true){
-            try {
-                System.out.println("THREAD A FUNCIONAR ");
-                socketSend.send(packetSend);
-                Thread.sleep(Constants.TIME);
-            } catch (IOException ex) {
-                Logger.getLogger(HeartbeatThreadSend.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(HeartbeatThreadSend.class.getName()).log(Level.SEVERE, null, ex);
-            }  
+        try{
+            ObjectOutputStream os = new ObjectOutputStream(new
+                                    BufferedOutputStream(byteStream));
+            os.flush();
+            
+            MSG mesageSend = new MSG();
+            List<String> listSend = new ArrayList<>();
+            
+            //ADICIONAR OS COMANDS A LIST
+            listSend.add(Constants.HEARTBEAT_SERVER);
+            
+            //ADICIONAR A LIST A MSG
+            mesageSend.setCMD(listSend);
+            
+            //ENVIAR
+            os.writeObject((MSG)mesageSend);
+            os.flush();
+
+            byte[] sendBuf = byteStream.toByteArray();
+            
+            packetSend = new DatagramPacket(sendBuf, sendBuf.length, ip, Constants.SENDING_PORT);
+            socketSend.send(packetSend);
+            
+        } catch(IOException ex) {
+            ex.printStackTrace();
         }
+        System.out.println("Registration submitted...");
+        
     }
 }
